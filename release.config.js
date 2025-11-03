368d1d54a26bad46084b77500445c807fe321748
Sasha Milenkovic <sasha@legalbookkeeping.com>
2025-11-02 22:11:49 -0500

    feat: configure semantic-release to publish all workspace packages

--- release.config.js
+++ release.config.js
@@ -5,7 +5,19 @@ export default {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    "@semantic-release/npm",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false, // Don't publish root package (it's private)
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        // Publish all workspace packages
+        publishCmd: "pnpm -r publish --access public --no-git-checks",
+      },
+    ],
     [
       "@semantic-release/git",
       {
